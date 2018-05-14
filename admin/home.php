178d8c49c320eb5e6f05b52620bd225886a55f09
AjayVermaa <37044392+AjayVermaa@users.noreply.github.com>
2018-05-15 00:03:45 +0530

    Add files via upload

---
+++ admin/home.php
@@ -0,0 +1,45 @@
+<?php 
+	include('../functions.php');
+
+	if (!isAdmin()) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: ../login.php');
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hall Booking</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="index.html" class="logo">Project @<strong></strong> Presidency University</a>
+
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Welcome to Hall-Booking</h1>
+					</header>
+
+					<div class="flex ">
+
+						
+
+
+
+					</div>
+
+	</body>
+</html>
